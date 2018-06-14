e2e50978dd2db9328c091da2c0d8049ba7be2cd9
ANITHAS22 <39801902+ANITHAS22@users.noreply.github.com>
2018-06-14 22:07:08 +0530

    Odd.java

---
+++ Odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Odd
+{
+public static void main(String args[])
+{
+Scanner io=new Scanner(System.in);
+int  n=io.nextInt();
+if(n==0||n<0)
+{
+System.out.println("invalid");
+}
+else if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.ou.println("Odd");
+}
+}
+}
